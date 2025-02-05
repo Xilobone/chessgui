9f5f369ec9f46120ba29b8912ebedb43600bf2ba
Xilobone <xilobone@gmail.com>
2025-02-05 20:31:50 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using chess;
+public class Progam
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello world");
+        Board board = Board.startPosition();
+        board.display();
+    }
+}
